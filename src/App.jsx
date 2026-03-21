a36602ed4eb50c8debd75ba347291fd0b980e834
Dylan Hartland <dylanh@Dylans-Mac-mini.local>
2026-03-21 09:07:04 +0200

    Deriving and Outputing data dynamically based on state using the TabButtons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  function handleSelect(selectedButton) {}
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button.");
 
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -33,6 +39,13 @@ function App() {
             <TabButton onClick={() => handleSelect("props")}>Props</TabButton>
             <TabButton onClick={() => handleSelect("state")}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
