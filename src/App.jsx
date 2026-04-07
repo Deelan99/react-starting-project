ab4a4fcb38cca5b690522673f7651dfad78caa27
Dylan Hartland <dylanh@Dylans-Mac-mini.local>
2026-04-07 19:55:30 +0200

    Working on Multiple JSX slots, setting default prop values, and setting component types dynamically.

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,16 @@
-import { useState, Fragment } from "react";
+import { Fragment } from "react";
 
-import { CORE_CONCEPTS } from "./data";
-import { EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
   return (
     <Fragment>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onClick={() => handleSelect("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onClick={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onClick={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onClick={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {!selectedTopic ? (
-            <p>Please select a topic to view examples.</p>
-          ) : (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic]?.title}</h3>
-              <p>{EXAMPLES[selectedTopic]?.description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic]?.code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </Fragment>
   );
